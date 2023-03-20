99e490f143f694a1d776f93279954dbc96f3326e
PCarina23 <128194487+pcari23@users.noreply.github.com>
2023-03-20 12:40:43 -0300

    Add files via upload

---
+++ Funcioneskks.js
@@ -0,0 +1,30 @@
+function sumaTres(x) {
+    console.log(x + 3);
+}
+
+sumaTres(5)
+
+// PRIMERO
+
+function sumaTres(x) {
+    return x + 3
+};
+
+// SEGUNDO
+
+var sumaTres = function (x) {
+    return x + 3;
+}
+
+// TERCERO
+
+var sumaTres = function (x) {
+    return x + 3;
+};
+
+
+
+
+
+
+
---
+++ Operaciones.js
@@ -0,0 +1,30 @@
+console.log("HOLA " + "EMMA");
+var nombre = "Emma";
+var saludo = "holi " + nombre
+console.log(saludo)
+
+console.log(3 + 5);
+console.log(3 - 5)
+console.log(3 * 5)
+console.log(15 / 3)
+console.log(15 % 3)
+console.log(3 + 4 * 2 - 8);
+console.log(12 ** 2)
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
